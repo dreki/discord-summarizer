168579ba8caa2a14b1e015368470fd5b7e50262a
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-08-09 21:23:09 -0500

    Added options component with basic styling 🎨

---
+++ options.tsx
@@ -0,0 +1,13 @@
+import "./style.css"
+
+function Options(): JSX.Element {
+    return (
+        <div className="container mx-auto pt-16">
+            <h1 className="text-3xl font-bold">Discord Summarizer</h1>
+            <h2 className="text-xl mt-4">Options</h2>
+            
+        </div>
+    )
+}
+
+export default Options
