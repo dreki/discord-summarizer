09c3fd2854e0ae5eab004786d22e7e4cfd35601f
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-08-09 21:35:19 -0500

    Add input for OpenAI key

--- options.tsx
+++ options.tsx
@@ -3,9 +3,20 @@ import "./style.css"
 function Options(): JSX.Element {
     return (
         <div className="container mx-auto pt-16">
-            <h1 className="text-3xl font-bold">Discord Summarizer</h1>
-            <h2 className="text-xl mt-4">Options</h2>
-            
+            <h1 className="text-3xl">
+                <span className="font-bold">Discord Summarizer</span> Options
+            </h1>
+
+            <div className="form-control mt-4">
+                <label className="label">
+                    <span className="label-text">OpenAI GPT API key</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered w-full max-w-xs"
+                />
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,5 @@ module.exports = {
     mode: "jit",
     darkMode: "class",
     content: ["./**/*.tsx"],
-    plugins: [require("@tailwindcss/typography")]
+    plugins: [require("@tailwindcss/typography"), require("daisyui")]
 }
