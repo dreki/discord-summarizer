bda4319d6f69c6a4322692c87df399850df94849
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-08-15 20:26:20 -0500

    📝 Updated options.tsx to include a space between "Discord Summarizer" and "Options" in the header.

--- options.tsx
+++ options.tsx
@@ -4,7 +4,7 @@ function Options(): JSX.Element {
     return (
         <div className="container mx-auto pt-16">
             <h1 className="text-3xl">
-                <span className="font-bold">Discord Summarizer</span> Options
+                <span className="font-bold">Discord Summarizer</span> <span className="ml-1">Options</span>
             </h1>
 
             <div className="form-control mt-4">
