57ffe5432c1366464d4e3498cf74a2ac731d3e3b
Sean Gilbertson <sean.gilbertson@gmail.com>
2023-08-17 22:14:41 -0500

    ✨ Init fetchUnreads message handler

---
+++ background/messages/fetchUnreads.ts
@@ -0,0 +1,12 @@
+import type { PlasmoMessaging } from "@plasmohq/messaging"
+
+const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
+    //   const message = await querySomeApi(req.body.id)
+
+    res.send({
+        // message
+        hello: "neat"
+    })
+}
+
+export default handler
